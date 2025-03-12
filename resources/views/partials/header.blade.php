402159ef69bb440b3809c12caabca5069e06730c
svan23 <vansla>
2025-03-11 23:32:47 -0700

    init project

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-light bg-light">
+    <div class="container">
+        <a href="#" class="navbar-brand">Laravel Guide</a>
+        <ul class="nav d-flex">
+            <li class="nav-item"><a href="#" class="nav-link">Products</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">About us</a></li>
+        </ul>
+    </div>
+</nav>
