8dd4fd6ed01f5f0ae62285aa981b4ea305508746
svan23 <vansla>
2025-03-13 21:50:30 -0700

    added filter logic to display article within start day and end day

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -12,9 +12,14 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        $data = Article::all();
-        // dd($data);
-        return view("articles.index", ['articles' => $data]);
+        $today = date("Y-m-d");
+
+        // Filter articles where the current date is within the start and end dates
+        $articles = Article::where('start_date', '<=', $today)
+                           ->where('end_date', '>=', $today)
+                           ->get();
+
+        return view('articles.index', ['articles' => $articles]);
     }
 
     /**
