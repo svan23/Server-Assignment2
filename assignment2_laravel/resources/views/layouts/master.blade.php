51ff78380248965184f248ad54e205b639df2f60
ZhaoqiuD <dingzq0807@gmail.com>
2025-03-28 21:40:27 -0700

    fixed minor issue of navbar

--- assignment2_laravel/resources/views/layouts/master.blade.php
+++ assignment2_laravel/resources/views/layouts/master.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
     <link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
 </head>
